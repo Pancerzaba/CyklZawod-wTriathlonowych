624bfd78e9c54a7714a0f6a45717e449ee09699a
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-01-15 23:44:57 +0100

    add class: Wolontariuszz, Zawody

---
+++ CyklZawodówTriathlonowych/Zawody.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyklZawodówTriathlonowych
+{
+    class Zawody
+    {
+        string miejsce;
+        string data;
+        int iloscNumerowStartowych;
+
+        public Zawody(string miejsce, string data, int iloscNumerowStartowych)
+        {
+            this.miejsce = miejsce;
+            this.data = data;
+            this.iloscNumerowStartowych = iloscNumerowStartowych;
+        }
+
+        enum DlugoscTrasy{Rekrut, Weteran }
+    }
+}
