fbc7d6de35cc4209db663e49ba790c99ea23a85c
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-01-21 22:06:24 +0100

    dodanie prototypow interfejsow

---
+++ CyklZawodówTriathlonowych/Organizator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyklZawodówTriathlonowych
+{
+    class Organizator : Osoba
+    {
+        public Organizator(string imie, string nazwisko, string nr_tel, string adres, int rok_ur) : base(imie, nazwisko, nr_tel, adres, rok_ur)
+        {
+        }
+    }
+}
