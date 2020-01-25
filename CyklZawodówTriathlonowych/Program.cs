a3ba784044c5e6dd3a4469e818c252c0e7eb324c
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-01-25 01:14:02 +0100

    aktualizacja wczytywania

--- CyklZawodówTriathlonowych/Program.cs
+++ CyklZawodówTriathlonowych/Program.cs
@@ -16,12 +16,23 @@ namespace CyklZawodówTriathlonowych
             IZawodnika osoba3 = new Zawodnik("Arek", "miś", "505453", "Wilno", 1946);
 
             IZawody zawody1 = new Zawody("służewiec", "duze", 10000, "Maraton XLP", "Tor Wyscigow Konnych", "15.06.2020", 100);
+            IZawody zawody2 = new Zawody("kortowo", "male", 200, "Kormoran", "Plaza", "15.10.2020", 10);
 
             osoba2.WyswietlDaneOsobowe();
             osoba3.WyswietlDaneOsobowe();
             osoba3.ZapiszSie();
             osoba3.IleLat();
+            osoba3.ZobaczZawody();
+            Dictionary<string, IZawody> Zawodow = new Dictionary<string, IZawody>();
 
+            Zawodow.Add("Maraton XLP",zawody1);
+            Zawodow.Add("Kormoran",zawody2);
+
+            for (int i = 0; i < Zawodow.Count; i++)
+            {
+                Console.WriteLine("Nazwa: {0}",
+                                                        Zawodow.Keys.ElementAt(i));
+            }
 
 
 
